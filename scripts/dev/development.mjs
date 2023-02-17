319f29beae9b86a478a392a4046a7e756029dc9f
linonetwo <linonetwo012@gmail.com>
2023-02-17 10:12:49 +0800

    chore: doc mode

--- scripts/dev/development.mjs
+++ scripts/dev/development.mjs
@@ -71,7 +71,11 @@ const refresh = async () => {
   }
   if (twServer) twServer.close();
   $tw2 = tw.TiddlyWiki();
-  $tw2.boot.argv = ['dist', '--listen'];
+  if (process.env.NODE_ENV === 'DOCUMENT') {
+    $tw2.boot.argv = ['+plugins/tiddlywiki/filesystem', '+plugins/tiddlywiki/tiddlyweb', 'demo', '--listen'];
+  } else {
+    $tw2.boot.argv = ['dist', '--listen'];
+  }
   $tw2.preloadTiddler({ title: '$:/Modern.TiddlyDev/devWebsocket/port', text: `${wssPort}` });
   $tw2.preloadTiddler({
     title: '$:/Modern.TiddlyDev/devWebsocket/listener',
